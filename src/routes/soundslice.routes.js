cfac40397d7bdadf82c46a99b38eb9c60de3ef6f
FaridaNelson <faridanelson@gmail.com>
2025-09-29 06:25:19 -0700

    added the soundslice api, connecting the tools for the profile

--- src/routes/soundslice.routes.js
+++ src/routes/soundslice.routes.js
@@ -1,7 +1,18 @@
 import { Router } from "express";
 import { requireAuth } from "../middleware/auth.js";
-import { getDailySlice } from "../controllers/soundslice.controller.js";
+import {
+  getDailySlice,
+  listSlices,
+  getSlice,
+  createSlice,
+} from "../controllers/soundslice.controller.js";
 
 const r = Router();
+
+r.get("/", requireAuth, listSlices);
+r.get("/:id", requireAuth, getSlice);
+r.post("/", requireAuth, createSlice);
+
 r.get("/daily", requireAuth, getDailySlice);
+
 export default r;
